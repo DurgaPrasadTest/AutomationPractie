0a3777f35f7c01f9ec3b84a0c36fcb983a48da24
Durga Prasad <siramdasudurgaprasad@gmail.com>
2025-10-26 16:31:33 +0530

    26/10/2025

--- src/test/java/PageObjects/User_Registration_Page.java
+++ src/test/java/PageObjects/User_Registration_Page.java
@@ -3,6 +3,7 @@ package PageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class User_Registration_Page extends Base_Page {
 	
@@ -83,6 +84,131 @@ public class User_Registration_Page extends Base_Page {
 	@FindBy(xpath="//button[@data-qa=\"create-account\"]")
 	WebElement btnCreateaccount;
 	
+	@FindBy(xpath="//b[normalize-space()='Account Created!']")
+	WebElement txtMsgAccountCreated;
 	
+	@FindBy(xpath="//a[@data-qa=\"continue-button\"]")
+	WebElement btnContinue;
+	
+	@FindBy(xpath="//b[normalize-space()='TesterUser']")
+	WebElement txtUsername;
+	
+	public void verifylogo() {
+		logo.isDisplayed();
+	}
+	
+	public void clickOnSignup_login() {
+		linkOnSignup_Login.click();
+	}
+	
+	public void verifyaccountinformationform() {
+		txtMsg_EnterAccountInformation.getText();
+	}
+	
+	public void EnterName(String name) {
+		input_Name.sendKeys(name);
+	}
+	
+	public void EnterEmail(String email) {
+		input_Email.sendKeys(email);
+	}
+	
+	public void verifysigupform() {
+		txtMsgNewUserSignup.getText();
+	}
+	
+	public void clickOnTitle() {
+		radiBtnTitle.click();
+	}
+	
+	public void EnterPassword(String password) {
+		inputPassword.sendKeys(password);
+	}
+	
+	public void SelectDay(String day) {
+		Select Date=new Select(drpDay);
+		Date.selectByVisibleText(day);
+		
+	}
+	
+	public void SelectMonth(String month) {
+		Select Month=new Select(drpMonth);
+		Month.selectByVisibleText(month);
+		
+	}
+	
+	public void SelectYear(String year) {
+		Select Year=new Select(drpYear);
+		Year.selectByVisibleText(year);
+		
+	}
+	
+	public void clicknewsletter() {
+		checkboxSignupforournewsletter.click();
+	}
+	
+	public void clickspecicaloffers() {
+		checkboxReceivespecialoffersfromourpartners.click();
+	}
+	
+	public void EnterFirstName(String fname) {
+		inputFirstname.sendKeys(fname);
+	}
+	
+	public void EnterLastName(String lname) {
+		inputLastname.sendKeys(lname);
+	}
+	
+	public void EnterCompanyName(String companyname) {
+		inputCompanyName.sendKeys(companyname);
+	}
+	
+	public void EnterAdd1(String address1) {
+		inputaddress1.sendKeys(address1);
+	}
+	
+	public void EnterAdd2(String address2) {
+		inputaddress2.sendKeys(address2);
+	}
+	
+	public void SelectCountry(String country) {
+		Select Country=new Select(drpCountry);
+		Country.selectByVisibleText(country);
+		
+	}
+	
+	public void EnterState(String state) {
+		inputState.sendKeys(state);
+	}
+	
+	public void EnterCity(String city) {
+		inputCity.sendKeys(city);
+	}
+	
+	public void EnterZipcode(String Zcode) {
+		inputZipcode.sendKeys(Zcode);
+	}
+	
+	public void EnterMobileNumber(String phoneNumber) {
+		inputMobileNumber.sendKeys(phoneNumber);
+	}
+	
+	public void clickOnCreateaccount() {
+		btnCreateaccount.click();
+	}
+	
+	public void verifyaccountcreated() {
+		txtMsgAccountCreated.getText();
+	}
+	
+	public void clickOncontinue() {
+		btnContinue.click();
+	}
+	
+	public void verifyUserloggedin() {
+		txtUsername.getText();
+	}
+	
+
 }
 
